d981aba4fa63f78cc7567428492805d9bd261ca6
Bro Chenzox <spalnavy@gmail.com>
2021-01-17 23:31:27 +0300

    Use DataBinding

--- app/src/main/java/com/palchak/sergey/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/palchak/sergey/kotlincoroutines/MainActivity.kt
@@ -2,10 +2,20 @@ package com.palchak.sergey.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.palchak.sergey.kotlincoroutines.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.button.setOnClickListener {
+
+        }
     }
 }
