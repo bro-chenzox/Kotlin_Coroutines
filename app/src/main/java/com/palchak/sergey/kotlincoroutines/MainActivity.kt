d7ada3e2af76669de18da0b54c0a915eca3e81b1
Bro Chenzox <spalnavy@gmail.com>
2021-01-21 14:38:43 +0300

    Activate dataBinding

--- app/src/main/java/com/palchak/sergey/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/palchak/sergey/kotlincoroutines/MainActivity.kt
@@ -2,8 +2,12 @@ package com.palchak.sergey.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.palchak.sergey.kotlincoroutines.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
