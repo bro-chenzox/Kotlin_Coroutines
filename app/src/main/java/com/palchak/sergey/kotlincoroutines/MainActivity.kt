768ccfd6647ad734a22826d9f0980d5ac461be30
Bro Chenzox <spalnavy@gmail.com>
2021-01-21 15:35:08 +0300

    Add constants and initiate dataBinding

--- app/src/main/java/com/palchak/sergey/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/palchak/sergey/kotlincoroutines/MainActivity.kt
@@ -2,14 +2,21 @@ package com.palchak.sergey.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.palchak.sergey.kotlincoroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CompletableJob
+
+private const val PROGRESS_MAX = 100
+private const val PROGRESS_START = 0
+private const val JOB_TIME = 4000 // ms
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var job : CompletableJob
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
