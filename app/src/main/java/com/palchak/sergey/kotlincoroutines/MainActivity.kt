0920c018f6dca6c646099713d46cc4ba1709c860
Bro Chenzox <spalnavy@gmail.com>
2021-01-24 22:21:31 +0300

    Launching in parallel

--- app/src/main/java/com/palchak/sergey/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/palchak/sergey/kotlincoroutines/MainActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.palchak.sergey.kotlincoroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +19,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.button.setOnClickListener {
+            setNewText("Clicked!")
+
+            fakeApiRequest()
+        }
+    }
+
+    private fun fakeApiRequest() {
+        CoroutineScope(IO).launch {
+            val job1 = launch {
+                val time1 = measureTimeMillis {
+                    println("debug: Launching job1 in thread ${Thread.currentThread().name}")
+                    val result1 = getResult1FromApi()
+                    setTextOnMainThread(result1)
+                }
+                println("debug: Completed job1 in $time1 ms")
+            }
+            val job2 = launch {
+                val time2 = measureTimeMillis {
+                    println("debug: Launching job2 in thread ${Thread.currentThread().name}")
+                    val result2 = getResult2FromApi()
+                    setTextOnMainThread(result2)
+                }
+                println("debug: Completed job1 in $time2 ms")
+            }
+        }
     }
 
     private fun setNewText(input: String) {
-        binding.text.text = input
+        val newText = binding.text.text.toString() + "$input\n"
+        binding.text.text = newText
     }
 
     private suspend fun setTextOnMainThread(input: String) {
